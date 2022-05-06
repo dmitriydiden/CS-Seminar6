68ac6dc3677e65b2a051ec117b365f6720c63570
dmitry_di <dnd_d@mail.ru>
2022-05-06 15:44:38 +0300

    Написали программу реверса массива

---
+++ task01_array_reverse/Program.cs
@@ -0,0 +1,24 @@
+﻿// Реверс массива (перестановка элементов массива)
+using System;
+using System.Linq;
+Console.Clear();
+
+/*Вариант 1. 
+
+int[] Array =new int[5].Select(x=>x=new Random().Next(0,10)).ToArray();
+Console.WriteLine(String.Join(" ", Array));
+Console.WriteLine(String.Join(" ", Array.Reverse()));
+*/
+
+//Вариант 2.
+int length=10;
+int[] Array =new int[10].Select(x=>x=new Random().Next(0,20)).ToArray();
+Console.WriteLine(String.Join(" ", Array));
+
+for (int i = 0; i < length/2; i++)
+{
+    int k=Array[i];
+    Array[i]=Array[length-i-1];
+    Array[length-i-1]=k;
+}
+Console.WriteLine(String.Join(" ", Array));

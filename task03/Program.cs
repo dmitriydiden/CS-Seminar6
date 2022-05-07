b7156bf45a0071777db17c0bc0ada9104b7daa41
dmitry_di <dnd_d@mail.ru>
2022-05-07 11:22:09 +0300

    Написали программу перевода десятичного числа в двоичное

---
+++ task03/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача №42 Написать программу, которая будет преобразовывать десятичное числов в двоичное
+using System;
+using System.Linq;
+Console.Clear();
+//Вариант 1
+Console.Write("Введите десятичное число - ");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Convert.ToString(i,2));
+// Вариант 2
+Console.WriteLine(DecToNum(6,2));
+
+string DecToNum(int decNumber, int otherSystem)
+{
+    string res="";
+    string nums = "0123456789ABCDEFG";
+    while(decNumber>0)
+    {
+        int ost = decNumber/otherSystem;
+        res=nums[decNumber-otherSystem*ost]+res;
+        decNumber/=otherSystem;
+    }
+    return res;
+}
